c7d9943c1bf5ffeacefbbc0cd2fe4005f15d1396
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-03-22 17:00:45 +0530

    Shell Scripting Concepts

---
+++ frondend.sh
@@ -0,0 +1,14 @@
+script_location=$(pwd)
+
+yum install nginx -y
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+#As it dont have VI editor to edit the files, we create the file locally and copy to the location to where our scripts are exist.
+
+
+cp $script_location/files/nginx.roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl restart nginx
