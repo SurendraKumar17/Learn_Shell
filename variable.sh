37a9dec9a2064960855e7fafd6cee5aabb57c24a
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-03-22 15:34:18 +0530

    Shell Scripting Concepts

--- variable.sh
+++ variable.sh
@@ -5,3 +5,9 @@ class=devops
 
 echo trainer is $trainer
 echo class is $class
+
+
+#To get the dynamic data or to use the commands as variables if we know that is occur many times in a terminal/script
+
+date=$(date +%F)
+echo Hey , today date is $date
