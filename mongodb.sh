5f91050d91750fd8042ee518e15502b0b2ad6d8a
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-03-24 18:35:42 +0530

    Shell Scripting Concepts

--- mongodb.sh
+++ mongodb.sh
@@ -4,4 +4,7 @@ cp $script_location/Files/mongodb.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
 
+#Substitute the value in the file using SED editor
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 systemctl restart mongod
