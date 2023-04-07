337be008d8d397e1045893268b4c5aa6eaafcf43
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-04-07 11:29:52 +0530

    functions.sh

---
+++ functions.sh
@@ -0,0 +1,10 @@
+#funtions are like variables, when we assign a name to a set of commands is a function.
+
+#declare a function
+example(){
+  echo im a example funtions
+}
+
+#calling a function
+
+example
