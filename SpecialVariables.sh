aaa123abc1ad7e9c27699be2a3aa73089db458d9
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-04-07 23:02:56 +0530

    Inputs to the script and functions.

--- SpecialVariables.sh
+++ SpecialVariables.sh
@@ -1,6 +1,18 @@
+echo following values from script
 echo "0 - $0"
 echo "1 - $1"
 echo "2 - $2"
 echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
+
+echo following values from function
+example() {
+  echo "0 - $0"
+  echo "1 - $1"
+  echo "2 - $2"
+  echo "* - $*"
+  echo "@ - $@"
+  echo "# - $#"
+}
+example
