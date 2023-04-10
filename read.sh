3a363b21c6f5c580feef845e7567543e864ceeac
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-04-10 22:08:28 +0530

    .

---
+++ read.sh
@@ -0,0 +1,6 @@
+#while executing the script we ask the input from the user using read command. this we do if script is executed manually , otherwise it wont works for automation it breaks the automation and ends with failures.
+
+read -p ' Enter your name ' name
+
+echo your name -$name
+
